84170f1d54fe9078713aecd8afc2d4458b7ad502
AADITYA Goenka <aaditya.uzumaki@gmail.com>
2023-08-10 17:36:55 +0530

    a

--- wordpress/wp-content/plugins/RedNote/importer.php
+++ wordpress/wp-content/plugins/RedNote/importer.php
@@ -16,6 +16,9 @@ function my_csv_importer_import_page() {
 }
 
 function my_csv_importer_process_csv() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'imported_contacts';
+
     if ($_FILES['csv_file']['error'] == 0) {
         $file = fopen($_FILES['csv_file']['tmp_name'], 'r');
 
@@ -25,21 +28,35 @@ function my_csv_importer_process_csv() {
         while (($line = fgetcsv($file)) !== FALSE) {
             list($name, $mobile, $email, $level, $profile_picture_url) = $line;
 
-            // Create a new post for each line
-            $post_id = wp_insert_post([
-                'post_title'    => $name,
-                'post_content'  => '', // Add content if needed
-                'post_status'   => 'publish',
-                'post_type'     => 'post',
-            ]);
+            // Check for duplicate mobile and email
+            $existing_mobile = $wpdb->get_var($wpdb->prepare("SELECT COUNT(*) FROM $table_name WHERE mobile = %s", $mobile));
+            $existing_email = $wpdb->get_var($wpdb->prepare("SELECT COUNT(*) FROM $table_name WHERE email = %s", $email));
 
-            // Add custom fields
-            update_post_meta($post_id, 'mobile', $mobile);
-            update_post_meta($post_id, 'email', $email);
-            update_post_meta($post_id, 'level', $level);
-            
-            // Attach profile picture
-            media_sideload_image($profile_picture_url, $post_id, $name);
+            if (!$existing_mobile && !$existing_email) {
+                $wpdb->insert($table_name, [
+                    'name' => $name,
+                    'mobile' => $mobile,
+                    'email' => $email,
+                    'level' => $level,
+                    'profile_picture_url' => $profile_picture_url
+                ]);
+
+                // Create a new post for each line
+                $post_id = wp_insert_post([
+                    'post_title'    => $name,
+                    'post_content'  => '', // Add content if needed
+                    'post_status'   => 'publish',
+                    'post_type'     => 'post',
+                ]);
+
+                // Add custom fields
+                update_post_meta($post_id, 'mobile', $mobile);
+                update_post_meta($post_id, 'email', $email);
+                update_post_meta($post_id, 'level', $level);
+                
+                // Attach profile picture
+                media_sideload_image($profile_picture_url, $post_id, $name);
+            }
         }
 
         fclose($file);
@@ -49,3 +66,5 @@ function my_csv_importer_process_csv() {
 
 add_action('admin_post_process_csv', 'my_csv_importer_process_csv');
 add_action('admin_post_nopriv_process_csv', 'my_csv_importer_process_csv');
+
+?>
--- wordpress/wp-content/plugins/RedNote/my-csv-importer.php
+++ wordpress/wp-content/plugins/RedNote/my-csv-importer.php
@@ -3,13 +3,13 @@
  * Plugin Name: My CSV Importer
  * Description: A plugin to import data from CSV and display it as post types.
  * Version: 1.0
- * Author: Your Name
+ * Author: Death301
  */
 
 // Include other files
 include_once plugin_dir_path(__FILE__) . 'importer.php';
 include_once plugin_dir_path(__FILE__) . 'shortcodes.php';
-include_once plugin_dir_path(__FILE__) . 'filters.php';
+include_once plugin_dir_path(__FILE__) . 'contacts.php';
 
 // Enqueue Bootstrap scripts and styles
 function my_csv_importer_enqueue_scripts() {
@@ -17,13 +17,6 @@ function my_csv_importer_enqueue_scripts() {
     wp_enqueue_script('bootstrap-js', 'https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js', array('jquery'), '', true);
 }
 add_action('wp_enqueue_scripts', 'my_csv_importer_enqueue_scripts');
-
-// Activation hook to set up initial data
-register_activation_hook(__FILE__, 'my_csv_importer_activation');
-function my_csv_importer_activation() {
-  // You can set up default values or database tables here
-}
-// Add to the existing main.php file
 
 // Function to add top-level menu
 function my_csv_importer_admin_menu() {
@@ -55,4 +48,11 @@ function my_csv_importer_admin_menu() {
 }
 add_action('admin_menu', 'my_csv_importer_admin_menu');
 
+// Activation hook to set up initial data
+function my_csv_importer_activation() {
+  my_csv_importer_create_table();
+}
+register_activation_hook(__FILE__, 'my_csv_importer_activation');
+
+// Include table creation function here as previously given
 ?>

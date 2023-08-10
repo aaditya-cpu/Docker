0f46d206907d2a46dda15b1bb6794b24f9495c27
AADITYA Goenka <aaditya.uzumaki@gmail.com>
2023-08-10 17:36:49 +0530

    update

--- wordpress/wp-content/plugins/RedNote/contacts.php
+++ wordpress/wp-content/plugins/RedNote/contacts.php
@@ -1,23 +1,22 @@
 <?php
 function my_csv_importer_contacts_page() {
-    // Query to get the imported contacts
-    $query = new WP_Query([
-        'post_type' => 'post', // Adjust as needed
-    ]);
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'imported_contacts';
+    $contacts = $wpdb->get_results("SELECT * FROM $table_name");
 
     echo '<div class="wrap">';
     echo '<h2>View Contacts</h2>';
     echo '<table class="table">';
-    echo '<thead><tr><th>Name</th><th>Mobile</th><th>Email</th><th>Level</th></tr></thead>';
+    echo '<thead><tr><th>Name</th><th>Mobile</th><th>Email</th><th>Level</th><th>Profile Picture</th></tr></thead>';
     echo '<tbody>';
 
-    while ($query->have_posts()) {
-        $query->the_post();
+    foreach ($contacts as $contact) {
         echo '<tr>';
-        echo '<td>' . get_the_title() . '</td>';
-        echo '<td>' . get_post_meta(get_the_ID(), 'mobile', true) . '</td>';
-        echo '<td>' . get_post_meta(get_the_ID(), 'email', true) . '</td>';
-        echo '<td>' . get_post_meta(get_the_ID(), 'level', true) . '</td>';
+        echo '<td>' . $contact->name . '</td>';
+        echo '<td>' . $contact->mobile . '</td>';
+        echo '<td>' . $contact->email . '</td>';
+        echo '<td>' . $contact->level . '</td>';
+        echo '<td>' . $contact->profile_picture_url . '</td>';
         echo '</tr>';
     }
 
@@ -25,6 +24,9 @@ function my_csv_importer_contacts_page() {
     echo '</table>';
     echo '</div>';
 }
+include_once plugin_dir_path(__FILE__) . 'contacts.php';
+?>
+
 
 // Include contacts.php in main.php
-include_once plugin_dir_path(__FILE__) . 'contacts.php';
+

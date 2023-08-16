dd1cb07ba08c3b5db457e1c651bd1068c1eac2ac
AADITYA Goenka <aaditya.uzumaki@gmail.com>
2023-08-16 17:43:24 +0530

    redd

--- wordpress/wp-content/plugins/RedNote/importer.php
+++ wordpress/wp-content/plugins/RedNote/importer.php
@@ -1,53 +1,4 @@
 <?php
-// Function to process the imported CSV data
-function my_csv_importer_process_csv() {
-    global $wpdb;
-    $table_name = $wpdb->prefix . 'imported_contacts';
-
-    if ($_FILES['csv_file']['error'] == 0) {
-        $file = fopen($_FILES['csv_file']['tmp_name'], 'r');
-
-        // Skip header row
-        fgetcsv($file);
-
-        while (($line = fgetcsv($file)) !== FALSE) {
-            list($name, $mobile, $email, $level, $profile_picture_url) = $line;
-
-            // Check for duplicate mobile and email
-            $existing_mobile = $wpdb->get_var($wpdb->prepare("SELECT COUNT(*) FROM $table_name WHERE mobile = %s", $mobile));
-            $existing_email = $wpdb->get_var($wpdb->prepare("SELECT COUNT(*) FROM $table_name WHERE email = %s", $email));
-
-            if (!$existing_mobile && !$existing_email) {
-                $wpdb->insert($table_name, [
-                    'name' => $name,
-                    'mobile' => $mobile,
-                    'email' => $email,
-                    'level' => $level,
-                    'profile_picture_url' => $profile_picture_url
-                ]);
-
-                // Create a new post for each line
-                $post_id = wp_insert_post([
-                    'post_title'    => $name,
-                    'post_content'  => '', // Add content if needed
-                    'post_status'   => 'publish',
-                    'post_type'     => 'post',
-                ]);
-
-                // Add custom fields
-                update_post_meta($post_id, 'mobile', $mobile);
-                update_post_meta($post_id, 'email', $email);
-                update_post_meta($post_id, 'level', $level);
-                
-                // Attach profile picture
-                media_sideload_image($profile_picture_url, $post_id, $name);
-            }
-        }
-
-        fclose($file);
-        // Redirect to a confirmation page or display a success message
-    }
-}
 
 // Function to create the database table
 function my_csv_importer_create_table() {
@@ -62,7 +13,6 @@ function my_csv_importer_create_table() {
         mobile varchar(20) NOT NULL,
         email varchar(255) NOT NULL,
         level varchar(50) NOT NULL,
-        profile_picture_url varchar(255),
         PRIMARY KEY  (id)
     ) $charset_collate;";
 
@@ -70,7 +20,96 @@ function my_csv_importer_create_table() {
     dbDelta($sql);
 }
 
+function my_csv_importer_process_csv() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'imported_contacts';
+
+    if ($_FILES['csv_file']['error'] == 0) {
+        $file = fopen($_FILES['csv_file']['tmp_name'], 'r');
+
+        // Skip header row
+        fgetcsv($file);
+
+        $importCount = 0; // Initialize import count
+
+        while (($line = fgetcsv($file)) !== FALSE) {
+            list($name, $mobile, $email, $level) = $line;
+
+            // Check for duplicate mobile and email
+            $existing_mobile = $wpdb->get_var($wpdb->prepare("SELECT COUNT(*) FROM $table_name WHERE mobile = %s", $mobile));
+            $existing_email = $wpdb->get_var($wpdb->prepare("SELECT COUNT(*) FROM $table_name WHERE email = %s", $email));
+
+            if (!$existing_mobile && !$existing_email) {
+                $wpdb->insert($table_name, [
+                    'name' => $name,
+                    'mobile' => $mobile,
+                    'email' => $email,
+                    'level' => $level
+                ]);
+
+                $importCount++; // Increment import count
+            }
+        }
+
+        fclose($file);
+
+        // Log import results
+        if ($importCount > 0) {
+            error_log("CSV imported successfully. Count: $importCount");
+        } else {
+            error_log("CSV did not upload, please check count - 0");
+        }
+    }
+}
+
+function create_members_from_db() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'imported_contacts';
+
+    $results = $wpdb->get_results("SELECT * FROM $table_name");
+
+    foreach ($results as $row) {
+        $post_data = array(
+            'post_title'    => wp_strip_all_tags($row->name),
+            'post_content'  => '', // Add content if required
+            'post_status'   => 'publish',
+            'post_author'   => 1, // Author ID
+            'post_type'     => 'members',
+            // Additional fields here
+        );
+
+        $post_id = wp_insert_post($post_data);
+
+        if ($post_id) {
+            // Set the taxonomy (level) if needed
+            wp_set_object_terms($post_id, $row->level, 'level', false);
+            
+            // Add custom fields if required
+            add_post_meta($post_id, 'mobile', $row->mobile);
+            add_post_meta($post_id, 'email', $row->email);
+        }
+    }
+}
+
+function my_csv_importer_import_page() {
+    ?>
+    <div class="container mt-5">
+        <h1>CSV Import</h1>
+        <p>Select a CSV file to import contacts:</p>
+        <form action="<?php echo admin_url('admin-post.php'); ?>" method="post" enctype="multipart/form-data">
+            <input type="hidden" name="action" value="process_csv">
+            <div class="mb-3">
+                <label for="csv_file" class="form-label">CSV File:</label>
+                <input class="form-control" type="file" id="csv_file" name="csv_file" accept=".csv" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Upload</button>
+        </form>
+    </div>
+    <?php
+}
+
 // Hook for processing CSV data
 add_action('admin_post_process_csv', 'my_csv_importer_process_csv');
 add_action('admin_post_nopriv_process_csv', 'my_csv_importer_process_csv');
+
 ?>
--- wordpress/wp-content/plugins/RedNote/main.php
+++ wordpress/wp-content/plugins/RedNote/main.php
@@ -1,19 +1,69 @@
 <?php
 /**
- * Plugin Name: My CSV Importer
- * Description: A plugin to import data from CSV and display it as post types.
+ * Plugin Name: Members and CSV Importer
+ * Description: A plugin to manage members with levels and import data from CSV.
  * Version: 1.0
  * Author: Death301
  */
 
-// Include other files
+// Register Custom Post Type Members
+function create_members_cpt() {
+  $labels = array(
+    'name' => _x('Members', 'Post Type General Name', 'textdomain'),
+    // ... more labels here ...
+  );
+  $args = array(
+    'label' => __('Members', 'textdomain'),
+    'labels' => $labels,
+    'supports' => array('title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields'),
+    'taxonomies' => array('level'),
+    'public' => true, // Make it publicly queryable
+    'show_ui' => true, // Show in admin UI
+    'show_in_menu' => true, // Show in admin sidebar
+    'menu_position' => 5, // Position in menu
+    'show_in_admin_bar' => true,
+    'show_in_nav_menus' => true,
+    'can_export' => true,
+    'has_archive' => true,
+    'hierarchical' => false,
+    'exclude_from_search' => false,
+    'show_in_rest' => true,
+    'capability_type' => 'post',
+);
+register_post_type('members', $args);
+
+}
+add_action('init', 'create_members_cpt', 0);
+
+// Register Custom Taxonomy Level
+function create_level_taxonomy() {
+  $labels = array(
+    'name' => _x('Levels', 'Taxonomy General Name', 'textdomain'),
+    // ... more labels here ...
+  );
+  $args = array(
+    'labels' => $labels,
+    'hierarchical' => true,
+    'public' => true,
+    'show_ui' => true,
+    'show_admin_column' => true,
+    'show_in_nav_menus' => true,
+    'show_tagcloud' => true,
+  );
+  register_taxonomy('level', array('members'), $args);
+}
+add_action('init', 'create_level_taxonomy', 0);
+
+// Include other necessary files
 include_once plugin_dir_path(__FILE__) . 'importer.php';
 
 // Activation hook to set up initial data
 register_activation_hook(__FILE__, 'my_csv_importer_activation');
 function my_csv_importer_activation() {
     my_csv_importer_create_table();
+    // my_csv_importer_process_csv(); // No need to call process_csv here; it will be triggered manually by the user
 }
+
 
 // Enqueue Bootstrap scripts and styles
 function my_csv_importer_enqueue_scripts() {
@@ -22,60 +72,29 @@ function my_csv_importer_enqueue_scripts() {
 }
 add_action('wp_enqueue_scripts', 'my_csv_importer_enqueue_scripts');
 
-// Function to add a top-level menu option for importing CSV
-function my_add_csv_import_menu_option() {
+
+function my_csv_importer_get_contacts() {
+  global $wpdb;
+  $table_name = $wpdb->prefix . 'imported_contacts';
+  $contacts = $wpdb->get_results("SELECT * FROM $table_name");
+
+  foreach ($contacts as $contact) {
+      echo '<p>' . $contact->name . ' - ' . $contact->email . '</p>';
+  }
+}
+
+
+// Add a menu page for CSV import
+function my_csv_importer_menu() {
     add_menu_page(
-        'CSV Import',     // Page title
-        'CSV Import',     // Menu title
-        'manage_options', // Capability required to access
-        'csv-import',     // Menu slug
-        'my_csv_importer_import_page', // Callback function
-        'dashicons-upload', // Menu icon
-        25               // Menu position
+        'CSV Import',
+        'CSV Import',
+        'manage_options',
+        'my-csv-importer',
+        'my_csv_importer_import_page',
+        'dashicons-upload',
+        25
     );
 }
-add_action('admin_menu', 'my_add_csv_import_menu_option');
-
-// Function to render the import page
-function my_csv_importer_import_page() {
-    ?>
-    <div class="wrap">
-        <h2>CSV Import</h2>
-        <form id="csv-import-form" method="post" enctype="multipart/form-data" class="form-inline">
-            <input type="hidden" name="action" value="process_csv">
-            <div class="form-group">
-                <input type="file" name="csv_file" class="form-control-file" accept=".csv" required>
-            </div>
-            <input type="submit" value="Upload CSV" class="btn btn-primary">
-        </form>
-        <div id="import-progress"></div>
-    </div>
-
-    <script>
-        jQuery(document).ready(function($) {
-            $('#csv-import-form').submit(function(event) {
-                event.preventDefault();
-                var formData = new FormData(this);
-
-                $.ajax({
-                    url: ajaxurl, // WordPress AJAX URL
-                    type: 'POST',
-                    data: formData,
-                    processData: false,
-                    contentType: false,
-                    beforeSend: function() {
-                        $('#import-progress').html('Importing... Please wait.');
-                    },
-                    success: function(response) {
-                        $('#import-progress').html(response);
-                    },
-                    error: function() {
-                        $('#import-progress').html('Error occurred during import.');
-                    }
-                });
-            });
-        });
-    </script>
-    <?php
-}
+add_action('admin_menu', 'my_csv_importer_menu');
 ?>
